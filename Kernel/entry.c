d4d14fd95def527c4ffe9347ca839530f68776c3
M4x1m3 <M4x1me@protonmail.com>
2021-11-27 14:47:26 +0100

    Added serial port driver

--- Kernel/entry.c
+++ Kernel/entry.c
@@ -1,4 +1,5 @@
 #include <Device/TTY.h>
+#include <Device/COM.h>
 #include <CPU/IDT.h>
 #include <Device/PIT.h>
 #include <CPU/ISR.h>
@@ -13,6 +14,9 @@ void k_entry(const void* multiboot_info)
 
     const char* str = "Je suis un test !\n";
     TTY_puts(str);
+
+    COM_init();
+    COM_puts(str);
 
     while (TRUE)
         __asm__ __volatile__("hlt");
